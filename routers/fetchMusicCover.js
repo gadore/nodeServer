bae2aefb7461767d7cec8aa44c33b4f32c8283c5
qianlicao <qianlizhihao@gmail.com>
2020-03-20 23:21:57 +0800

    fix bugs

--- routers/fetchMusicCover.js
+++ routers/fetchMusicCover.js
@@ -3,7 +3,6 @@ const Logger = require('../handler/logger')
 const fs = require('fs')
 var cheerio = require('cheerio')
 
-<<<<<<< HEAD
 function fetMusicCover(res, req) {
     req.on('data', function (data) {
         try {
@@ -35,28 +34,6 @@ function fetMusicCover(res, req) {
                     }
                     res.write(JSON.stringify(result))
                     res.end()
-=======
-function fetMusicCover(res,req) {
-    req.on('data',function(data){
-        Logger.getInstance().logInfo('fetchCover',data.toString())
-        var url = JSON.parse(data).url
-        request(url, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                fs.writeFileSync('result.html',body,function(e){
-                    if(e){
-                        console.log(e)
-                    }
-                    console.log('save success !')
-                })
-                var $ = cheerio.load(body)
-                // <meta property="og:image" content="http://p2.music.126.net/LGSZ3rGT8Ux1pYxcwxnR-g==/2225411534621328.jpg" />
-                var tags = $('meta[property="og:image"]')
-                var imgUrl = tags.attr('content')
-                res.writeHead(200)
-                var result = {
-                    message:'success',
-                    data:imgUrl
->>>>>>> ad041da9e25429e10b552fcb1a9a2a51ed4c697a
                 }
             })
         } catch (e) {
