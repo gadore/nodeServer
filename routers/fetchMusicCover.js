5f64687d09513e414ac357ca66381f26e90623ec
qianlicao <qianlizhihao@gmail.com>
2020-03-21 15:35:16 +0800

    fixbugs

--- routers/fetchMusicCover.js
+++ routers/fetchMusicCover.js
@@ -3,42 +3,33 @@ const Logger = require('../handler/logger')
 const fs = require('fs')
 var cheerio = require('cheerio')
 
-function fetMusicCover(res, req) {
-    req.on('data', function (data) {
-        try {
-            Logger.getInstance().logInfo('fetchCover', data.toString())
-            var url = JSON.parse(data).url
-            request(url, function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    var $ = cheerio.load(body)
-                    var tags = $('ul.f-hide')
-                    var songs = []
-                    tags[0].childNodes.forEach(tag => {
-                        var music = new Object()
-                        music.name = tag.childNodes[0].childNodes[0].data
-                        music.id = tag.childNodes[0].attribs.href
-                        songs.push(music)
-                    });
-                    res.writeHead(200)
-                    var result = {
-                        message: 'success',
-                        data: songs
-                    }
-                    res.write(JSON.stringify(result))
-                    res.end()
-                } else {
-                    res.writeHead(200)
-                    var result = {
-                        message: 'fetch error',
-                        data: []
-                    }
-                    res.write(JSON.stringify(result))
-                    res.end()
+function fetMusicCover(res,req) {
+    req.on('data',function(data){
+        Logger.getInstance().logInfo('fetchCover',data.toString())
+        var url = JSON.parse(data).url
+        request(url, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var $ = cheerio.load(body)
+                // <meta property="og:image" content="http://p2.music.126.net/LGSZ3rGT8Ux1pYxcwxnR-g==/2225411534621328.jpg" />
+                var tags = $('meta[property="og:image"]')
+                var imgUrl = tags.attr('content')
+                res.writeHead(200)
+                var result = {
+                    message:'success',
+                    data:imgUrl
                 }
-            })
-        } catch (e) {
-            Logger.getInstance().logError('fetchMusicCover', e)
-        }
+                res.write(JSON.stringify(result))
+                res.end()
+            }else{
+                res.writeHead(200)
+                var result = {
+                    message:'fetch error',
+                    data:[]
+                }
+                res.write(JSON.stringify(result))
+                res.end()
+            }
+        })
     })
 }
 
